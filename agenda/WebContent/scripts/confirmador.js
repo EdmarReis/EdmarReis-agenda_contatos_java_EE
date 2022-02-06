7607607645a62b87ff6f6b418180dbb8e761a863
Edmar Reis <91325197+EdmarReis@users.noreply.github.com>
2022-02-06 12:10:01 -0300

    Projeto agenda adicionado

---
+++ agenda/WebContent/scripts/confirmador.js
@@ -0,0 +1,13 @@
+/**
+ * Confirmação de exclusão de um contato
+ * 
+ * @author Edmar Reis
+ */
+
+function confirmar(idcon) {
+	let resposta = confirm("Confirma a exclusão desse contato?")
+	if (resposta === true) {
+		//alert(idcon)
+		window.location.href = "delete?idcon=" + idcon
+	}
+}
